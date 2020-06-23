1c69f2750880088b6e6200867f63b242d441d9fc
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2020-06-23 10:29:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+const servers = [];
+
+io.sockets.on('connection', function(socket) {
+    socket.on('connector', function(obj) {
+        if (servers.includes(obj.chat)) {
+          socket.chat = obj.chat;
+        } else {
+          var server = {};
+          server.name = obj.chat;
+          server.users = 1;
+          server.owner = obj.username;
+          socket.chat = obj.chat;
+        }
+        io.emit('is_online', '<i>' + socket.username + ' has joined the chat..</i>', socket.chat);
+        console.log(" + " + socket.username);
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '<i>' + socket.username + ' left the chat..</i>', socket.chat);
+        console.log(" - " + socket.username)
+    })
+
+    // socket.on('is_typing', function(chat) {
+    //   io.emit('typing_noti', socket.username, chat)
+    // })
+
+    socket.on('chat_message', function(obj, chat) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong> | ' + obj, chat);
+    });
+
+    socket.on('create', function(name) {
+      if (servers.includes(obj.chat)) {
+        socket.emit('failure_chat_create');
+      } else {
+        var server = {};
+        server.name = obj.chat;
+        server.users = 1;
+        socket.chat = obj.chat;
+      }
+    });
+});
+
+const server = http.listen(1337, function() {
+    console.log('listening on *:1337');
+});
